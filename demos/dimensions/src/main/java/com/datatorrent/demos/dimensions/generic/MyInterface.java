37f4770268938bd3d369a343da52f3fb9299f05d
Ashwin Chandra Putta <ashwin@datatorrent.com>
2015-04-01 14:12:45 -0700

    added interface implementation using asm

---
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/generic/MyInterface.java
@@ -0,0 +1,14 @@
+/*
+ *  Copyright (c) 2012-2015 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.datatorrent.demos.dimensions.generic;
+
+/**
+ *
+ * @author Ashwin Chandra Putta <ashwin@datatorrent.com>
+ */
+public interface MyInterface
+{
+  public String getValue();
+}
